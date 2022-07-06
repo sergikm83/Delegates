09e9e0227085bf20d70e598b6b9e40ef55bd0145
Sergey Misko <spy_3462@mail.ru>
2022-07-06 17:00:09 +0400

    Добавил делегат в Car

--- CarDelegate/Car.cs
+++ CarDelegate/Car.cs
@@ -8,6 +8,15 @@ namespace CarDelegate
 {
     public class Car
     {
+        // Определить тип делегата
+        public delegate void CarEngineHandler(string msgForCaller);
+        // Определить переменную-член этого типа делегата
+        private CarEngineHandler listOfHandlers;
+        // Добавить регистрационную функцию для вызывающего кода
+        public void RegisterWithCarEngine(CarEngineHandler methodToCall)
+        {
+            listOfHandlers = methodToCall;
+        }
         public int CurrentSpeed { get; set; }
         public int MaxSpeed { get; set; } = 100;
         public string PetName { get; set; }
