a64a66dc6a97a953f8c3c88b7a062d267eb6babd
Sergey Misko <spy_3462@mail.ru>
2022-07-06 16:37:18 +0400

    добавил делегат BinaryOp добавил класс SimpleMath

--- SimpleDelegate/Program.cs
+++ SimpleDelegate/Program.cs
@@ -2,6 +2,13 @@
 
 namespace SimpleDelegate
 {
+    public delegate int BinatyOp(int x, int y);
+
+    public class SimpleMath
+    {
+        public static int Add(int x, int y) => x + y;
+        public static int Subtract(int x, int y) => x - y;
+    }
     class Program
     {
         static void Main(string[] args)
