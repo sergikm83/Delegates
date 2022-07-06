b116942aebee1a600e98d8c43cebab680a57c98c
Sergey Misko <spy_3462@mail.ru>
2022-07-06 17:49:22 +0400

    затестил класс Car с выводом сообщений

--- CarDelegate/Program.cs
+++ CarDelegate/Program.cs
@@ -6,7 +6,20 @@ namespace CarDelegate
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***** Delegates as event enablers *****\n");
+            Car c1 = new Car("SlugBug", 100, 10);
+            // Сообщить объекту Car, какой метод вызывать,
+            // когда он пожелает отправить сообщение
+            c1.RegisterWithCarEngine(new Car.CarEngineHandler(OnCarEngineEvent));
+            Console.WriteLine("***** Speeding up *****");
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
+        }
+        public static void OnCarEngineEvent(string msg)
+        {
+            Console.WriteLine("\n***** Message From Car Object *****");
+            Console.WriteLine("=> {0}", msg);
+            Console.WriteLine("************************************\n");
         }
     }
 }
