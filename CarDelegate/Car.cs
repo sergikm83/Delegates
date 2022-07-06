992de74294d5468d24d71f8dadc79bfb8cd8ac23
Sergey Misko <spy_3462@mail.ru>
2022-07-06 16:56:02 +0400

    добавил класс Car

---
+++ CarDelegate/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDelegate
+{
+    public class Car
+    {
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; } = 100;
+        public string PetName { get; set; }
+        // исправен ли автомобиль
+        private bool carIsDead;
+
+        public Car() { }
+        public Car(string name, int maxSp, int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = maxSp;
+            PetName = name;
+        }
+    }
+}
