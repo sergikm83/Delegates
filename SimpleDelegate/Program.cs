de6f54e3c72f11073e1c52fa2c50c10e096f9d47
Sergey Misko <spy_3462@mail.ru>
2022-07-06 16:42:59 +0400

    добавил метод SquareNumber в класс SimpleMath

--- SimpleDelegate/Program.cs
+++ SimpleDelegate/Program.cs
@@ -8,6 +8,7 @@ namespace SimpleDelegate
     {
         public static int Add(int x, int y) => x + y;
         public static int Subtract(int x, int y) => x - y;
+        public static int SquareNumber(int a) => a * a;
     }
     class Program
     {
