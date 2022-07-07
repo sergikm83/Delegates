163eb7f85b7d37528c0e5283bc752047a63fc16d
Sergey Misko <spy_3462@mail.ru>
2022-07-07 14:00:54 +0400

    добавил методы CarAboutToBlow, CarIsAlmostDoomed, CarExploded для обработчиков событий c1 и код в Main для проверки

--- CarEvents/Program.cs
+++ CarEvents/Program.cs
@@ -6,7 +6,28 @@ namespace CarEvents
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***** Fun with Events *****\n");
+            Car c1 = new Car("SlugBug", 100, 10);
+            // Зарегистрировать обработчики событий
+            c1.AboutToBlow += new Car.CarEngineHandler(CarIsAlmostDoomed);
+            c1.AboutToBlow += new Car.CarEngineHandler(CarAboutToBlow);
+            Car.CarEngineHandler d = new Car.CarEngineHandler(CarExploded);
+            c1.Exploded += d;
+            Console.WriteLine("***** Speeding up *****");
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
+            // Удалить метод CarExploded из списка вызовов.
+            c1.Exploded -= d;
+            Console.WriteLine("\n***** Speeding up *****");
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
         }
+
+        public static void CarAboutToBlow(string msg)
+            => Console.WriteLine(msg);
+        public static void CarIsAlmostDoomed(string msg)
+            => Console.WriteLine("=> Critical Message from Car: {0}",msg);
+        public static void CarExploded(string msg)
+            => Console.WriteLine(msg);
     }
 }
