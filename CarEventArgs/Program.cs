2ba758037d103d69d6749fc93ed3bbc7b24a82a4
Sergey Misko <spy_3462@mail.ru>
2022-07-07 15:31:51 +0400

    добавил код в Main для проверки работы

--- CarEventArgs/Program.cs
+++ CarEventArgs/Program.cs
@@ -6,7 +6,31 @@ namespace CarEventArgs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***** Fun with Events *****\n");
+            Car c1 = new Car("SlugBug", 100, 10);
+            // Зарегистрировать обработчики событий
+            c1.AboutToBlow += CarAboutToBlow;
+            c1.Exploded += CarExploded;
+            Console.WriteLine("***** Speeding up *****");
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
+            // Удалить метод CarExploded из списка вызовов.
+            c1.Exploded -= CarExploded;
+            Console.WriteLine("\n***** Speeding up *****");
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
         }
+
+        public static void CarAboutToBlow(object sender, CarEventArgs e)
+        { 
+            if (sender is Car c)
+            {
+                c = (Car)sender;
+                Console.WriteLine("Critical Message from {0}: {1}",c.PetName,e.msg);
+            }
+        }
+
+        public static void CarExploded(object sender, CarEventArgs e) => Console.WriteLine(e.msg);
+
     }
 }
