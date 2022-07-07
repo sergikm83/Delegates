354b34d7405454283abecf33bb10236601fd9f1d
Sergey Misko <spy_3462@mail.ru>
2022-07-07 12:45:45 +0400

    добавил новый метод DisplayMessage

--- ActionAndFuncDelegates/Program.cs
+++ ActionAndFuncDelegates/Program.cs
@@ -8,5 +8,18 @@ namespace ActionAndFuncDelegates
         {
             Console.WriteLine("Hello World!");
         }
+
+        static void DisplayMessage(string msg, ConsoleColor txtColor, int printCount)
+        {
+            ConsoleColor previous = Console.ForegroundColor;
+            Console.ForegroundColor = txtColor;
+
+            for (int i=0;i<printCount;i++)
+            {
+                Console.WriteLine(msg);
+            }
+
+            Console.ForegroundColor = previous;
+        }
     }
 }
