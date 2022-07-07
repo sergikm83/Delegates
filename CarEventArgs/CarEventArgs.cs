7d1edb8ae4e1e3d32f1ba13d436cc81765965f67
Sergey Misko <spy_3462@mail.ru>
2022-07-07 15:20:49 +0400

    добавил новый класс CarEventArgs

---
+++ CarEventArgs/CarEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarEventArgs
+{
+    public class CarEventArgs : EventArgs
+    {
+        public readonly string msg;
+        public CarEventArgs(string message) => msg = message;
+    }
+}
