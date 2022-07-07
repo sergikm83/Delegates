6abc0a165270daacb4c2842d677733755b1110e2
Sergey Misko <spy_3462@mail.ru>
2022-07-07 14:22:41 +0400

    сделал более читабельный вид регистрации событий

--- CarEvents/Program.cs
+++ CarEvents/Program.cs
@@ -9,15 +9,14 @@ namespace CarEvents
             Console.WriteLine("***** Fun with Events *****\n");
             Car c1 = new Car("SlugBug", 100, 10);
             // Зарегистрировать обработчики событий
-            c1.AboutToBlow += new Car.CarEngineHandler(CarIsAlmostDoomed);
-            c1.AboutToBlow += new Car.CarEngineHandler(CarAboutToBlow);
-            Car.CarEngineHandler d = new Car.CarEngineHandler(CarExploded);
-            c1.Exploded += d;
+            c1.AboutToBlow += CarIsAlmostDoomed;
+            c1.AboutToBlow += CarAboutToBlow;
+            c1.Exploded += CarExploded;
             Console.WriteLine("***** Speeding up *****");
             for (int i = 0; i < 6; i++)
                 c1.Accelerate(20);
             // Удалить метод CarExploded из списка вызовов.
-            c1.Exploded -= d;
+            c1.Exploded -= CarExploded;
             Console.WriteLine("\n***** Speeding up *****");
             for (int i = 0; i < 6; i++)
                 c1.Accelerate(20);
